6ee1520bb11193a2cb5d8bb841c2d0f981366aa7
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-04-24 23:49:25 +0600

    u

--- jobprotal-ui/src/components/Footer/Footer.jsx
+++ jobprotal-ui/src/components/Footer/Footer.jsx
@@ -24,23 +24,45 @@ const Footer = () => {
         </aside>
         <nav>
           <h6 className="footer-title">Services</h6>
-          <Link className="link link-hover">Branding</Link>
-          <Link className="link link-hover">Design</Link>
-          <Link className="link link-hover">Marketing</Link>
-          <Link className="link link-hover">Advertisement</Link>
+          <Link to="#" className="link link-hover">
+            Branding
+          </Link>
+          <Link to="#" className="link link-hover">
+            Design
+          </Link>
+          <Link to="#" className="link link-hover">
+            Marketing
+          </Link>
+          <Link to="#" className="link link-hover">
+            Advertisement
+          </Link>
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
-          <Link className="link link-hover">About us</Link>
-          <Link className="link link-hover">Contact</Link>
-          <Link className="link link-hover">Jobs</Link>
-          <Link className="link link-hover">Press kit</Link>
+          <Link to="#" className="link link-hover">
+            About us
+          </Link>
+          <Link to="#" className="link link-hover">
+            Contact
+          </Link>
+          <Link to="#" className="link link-hover">
+            Jobs
+          </Link>
+          <Link to="#" className="link link-hover">
+            Press kit
+          </Link>
         </nav>
         <nav>
           <h6 className="footer-title">Legal</h6>
-          <Link className="link link-hover">Terms of use</Link>
-          <Link className="link link-hover">Privacy policy</Link>
-          <Link className="link link-hover">Cookie policy</Link>
+          <Link to="#" className="link link-hover">
+            Terms of use
+          </Link>
+          <Link to="#" className="link link-hover">
+            Privacy policy
+          </Link>
+          <Link to="#" className="link link-hover">
+            Cookie policy
+          </Link>
         </nav>
       </footer>
     </>
