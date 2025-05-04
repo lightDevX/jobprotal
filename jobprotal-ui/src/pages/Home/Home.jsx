de7c457604721b21089278116f592aab077eb7b3
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-05-04 17:16:07 +0600

    u

--- jobprotal-ui/src/pages/Home/Home.jsx
+++ jobprotal-ui/src/pages/Home/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>This is Home Page data from home</div>;
+  return (
+    <>
+      <div>This is Home Page data from home</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+        pariatur maxime voluptas molestiae quidem esse quas fugiat. Quasi
+        placeat deleniti quae inventore dignissimos sit hic repellendus omnis
+        possimus. Reiciendis, ipsum?
+      </p>
+    </>
+  );
 };
 
 export default Home;
