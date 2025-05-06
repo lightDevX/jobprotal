6e1944eb676a24a16f425cca197af2530c6feb9f
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-05-06 23:54:42 +0600

    u

--- jobprotal-ui/src/pages/Home/Home.jsx
+++ jobprotal-ui/src/pages/Home/Home.jsx
@@ -8,6 +8,14 @@ const Home = () => {
         placeat deleniti quae inventore dignissimos sit hic repellendus omnis
         possimus. Reiciendis, ipsum?
       </p>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+          incidunt nesciunt quae non autem a adipisci veniam. Suscipit quidem
+          dignissimos cum veniam ipsam nihil optio sunt explicabo recusandae
+          aperiam. Dolores!
+        </p>
+      </div>
     </>
   );
 };
