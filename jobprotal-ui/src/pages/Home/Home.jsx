0e8f8a17af42cc5d90506789dc3a203e73266d46
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-05-07 17:55:06 +0600

    Hero section create

--- jobprotal-ui/src/pages/Home/Home.jsx
+++ jobprotal-ui/src/pages/Home/Home.jsx
@@ -1,21 +1,9 @@
+import Bannar from "./Bannar";
+
 const Home = () => {
   return (
     <>
-      <div>This is Home Page data from home</div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-        pariatur maxime voluptas molestiae quidem esse quas fugiat. Quasi
-        placeat deleniti quae inventore dignissimos sit hic repellendus omnis
-        possimus. Reiciendis, ipsum?
-      </p>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-          incidunt nesciunt quae non autem a adipisci veniam. Suscipit quidem
-          dignissimos cum veniam ipsam nihil optio sunt explicabo recusandae
-          aperiam. Dolores!
-        </p>
-      </div>
+      <Bannar></Bannar>
     </>
   );
 };
