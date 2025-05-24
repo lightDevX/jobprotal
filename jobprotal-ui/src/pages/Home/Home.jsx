e763e29ab6c0803ad0f4128f7790eb49d8a49223
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-05-24 18:25:43 +0600

    job card added

--- jobprotal-ui/src/pages/Home/Home.jsx
+++ jobprotal-ui/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import HotJobs from "../../components/HotJobs/HotJobs";
 import Bannar from "./Bannar";
 
 const Home = () => {
   return (
     <>
       <Bannar></Bannar>
+      <HotJobs />
     </>
   );
 };
